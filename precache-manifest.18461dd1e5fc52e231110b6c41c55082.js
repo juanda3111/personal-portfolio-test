0e534524cc83a3bebf4ff8c17a320234522c6578
juanda3111 <you@example.com>
2020-01-21 14:26:29 -0500

    Updates

---
+++ precache-manifest.18461dd1e5fc52e231110b6c41c55082.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "588c05d25928fe102c9ea3558df1dd58",
+    "url": "/personal-portfolio-test/index.html"
+  },
+  {
+    "revision": "46d691d4d13ced6e7e06",
+    "url": "/personal-portfolio-test/static/css/2.a6bc1c31.chunk.css"
+  },
+  {
+    "revision": "0ea3cbecf8c3e3941258",
+    "url": "/personal-portfolio-test/static/css/main.c72e38d6.chunk.css"
+  },
+  {
+    "revision": "46d691d4d13ced6e7e06",
+    "url": "/personal-portfolio-test/static/js/2.58dded4a.chunk.js"
+  },
+  {
+    "revision": "c7abd4a823ee85afac2f05677368b7de",
+    "url": "/personal-portfolio-test/static/js/2.58dded4a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0ea3cbecf8c3e3941258",
+    "url": "/personal-portfolio-test/static/js/main.339f4037.chunk.js"
+  },
+  {
+    "revision": "90e63473fa6a9644a703",
+    "url": "/personal-portfolio-test/static/js/runtime-main.6cee8ddd.js"
+  }
+]);

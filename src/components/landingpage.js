40c6d7256079f3d430a89e7fc727ddddd25fa479
juanda3111 <you@example.com>
2020-01-21 19:43:24 -0500

    arreglo responsive landing

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -4,42 +4,47 @@ import { Grid, Cell } from 'react-mdl';
 class Landing extends Component {
   render() {
     return (
-      <div style={{width:'100%', margin: 'auto'}}>
-        <Grid className='landing-grid'>
-          <Cell col={12}>
-            <img src="https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png"
-                 alt="avatar"
-                 className="avatar-img"
-              />
-            <div className="banner-text">
-              <h1>Full Stack Web Developer</h1>
-              <hr/>
+      <div className="container">
+        <div className="row">
+          <div style={{width:'100%', margin: 'auto'}}>
+            <Grid className='landing-grid'>
+              <Cell col={12}>
+                <img src="https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png"
+                     alt="avatar"
+                     className="avatar-img"
+                  />
+                <div className="banner-text">
+                  <h1 >Full Stack Web Developer</h1>
+                  <hr/>
 
-              <p>html/css | Bootstrap | JavaScript | React | NodeJS | php | Wordpress | Express | MySQL | MongoDB </p>
-              <div className="social-links">
-                {/*LinkedIn*/}
-                <a href="http://google.com" rel="noopener noreferrer" target="_blank">
-                  <i className="fa fa-linkedin-square" aria-hidden="true"/>
-                </a>
-                {/*Github*/}
-                <a href="http://google.com" rel="noopener noreferrer" target="_blank">
-                  <i className="fa fa-github-square" aria-hidden="true"/>
-                </a>
-                {/*CodePen*/}
-                <a href="http://google.com" rel="noopener noreferrer" target="_blank">
-                  <i className="fa fa-free-code-camp" aria-hidden="true"/>
-                </a>
-                {/*FreeCodeCamp*/}
-                <a href="http://google.com" rel="noopener noreferrer" target="_blank">
-                  <i className="fa fa-codepen" aria-hidden="true"/>
-                </a>
+                  <p>html/css | Bootstrap | JavaScript | React | NodeJS | php | Wordpress | Express | MySQL | MongoDB </p>
+                  <div className="social-links">
+                    {/*LinkedIn*/}
+                    <a href="http://google.com" rel="noopener noreferrer" target="_blank">
+                      <i className="fa fa-linkedin-square" aria-hidden="true"/>
+                    </a>
+                    {/*Github*/}
+                    <a href="http://google.com" rel="noopener noreferrer" target="_blank">
+                      <i className="fa fa-github-square" aria-hidden="true"/>
+                    </a>
+                    {/*CodePen*/}
+                    <a href="http://google.com" rel="noopener noreferrer" target="_blank">
+                      <i className="fa fa-free-code-camp" aria-hidden="true"/>
+                    </a>
+                    {/*FreeCodeCamp*/}
+                    <a href="http://google.com" rel="noopener noreferrer" target="_blank">
+                      <i className="fa fa-codepen" aria-hidden="true"/>
+                    </a>
 
-              </div>
-            </div>
+                  </div>
+                </div>
 
-          </Cell>
-        </Grid>
+              </Cell>
+            </Grid>
+          </div>
+        </div>
       </div>
+
     )
   }
 }
